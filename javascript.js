d144fe8607f18e9d07bdb075759ad3ac60c2ddcb
James Snell <Vulpes3.1415@yahoo.com>
2019-09-23 22:27:23 -0600

    Set cdn links up

---
+++ javascript.js
@@ -0,0 +1,24 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAxq3aGL4DN8D7jvWUsiZYMZu3_Q1sp9DY",
+    authDomain: "train-conductor.firebaseapp.com",
+    databaseURL: "https://train-conductor.firebaseio.com",
+    projectId: "train-conductor",
+    storageBucket: "",
+    messagingSenderId: "768965909493",
+    appId: "1:768965909493:web:0a6f66e7d66a4b594c9ebc"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var trainName = "";
+var	destination = "";
+var	frequency = "";
+var nextArrival	= "";
+
+$("#addTrain").on("click", function(){
+    trainName = $("#trainNameInput").val().trim();
+    destination = $("#destinationInput").val().trim();
+    frequency = $("#frequencyInput").val().trim();
+    nextArrival = $("#nextArrivalInput").val().trim();
+})
